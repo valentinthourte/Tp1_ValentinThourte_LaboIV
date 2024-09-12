f95e39686646b349587b4b60f03b84ea472bc988
vthourte <valentinthourte@gmail.com>
2024-09-12 18:40:37 -0300

    Implementacion sprint 1

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { QuienSoyComponent } from './quien-soy/quien-soy.component';
+import { LoginComponent } from './login/login.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', redirectTo: '/login', pathMatch: "full"},
+    {path: 'home', component: HomeComponent},
+    {path: 'quiensoy', component: QuienSoyComponent},
+    {path: 'login', component: LoginComponent}
+];
